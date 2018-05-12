7d1553e1d4485907c8ecb11df88ac3285a38b628
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-05-12 23:00:44 +0300

    Added menu validation

--- src/main/java/menuPackage/MainMenu.java
+++ src/main/java/menuPackage/MainMenu.java
@@ -1,56 +1,76 @@
 package menuPackage;
 import java.util.Scanner;
-
+import service.CompanyDirectoryService;
 public class MainMenu extends AbstractMenu {
-
+    private CompanyDirectoryService service;
     public MainMenu() {
         super();
         initMenuItems();
+        this.service= new CompanyDirectoryService();
     }
-
     private void initMenuItems() {
         this.items.add("Download file");
         this.items.add("Search clients");
         this.items.add("Sort clients");
     }
-
     @Override
     public void interactWithMenu() {
-//TODO: Обработка ввода и вызов метода контроллера
-        Scanner sc1 = new Scanner(System.in);
+    //TODO: Обработка ввода и вызов метода контроллера
+       // Scanner sc = new Scanner(System.in);
         boolean exit = false;
         do {
             printMenu(); //вызываю печать меню от menu.AbstractMenu
-            System.out.println("Ввод пункта меню с клавиатуры: ");
-            int command = sc1.nextInt();//
-            switch (command) {
-                case 1:
-                    System.out.println("Вы ввели число 1 - Загрузка файла ");
-                    Scanner scName = new Scanner(System.in);
-                    String nameFromKeyBoard = scName.nextLine();
+            Scanner sc = new Scanner(System.in);
+            int command;
+            do {
+                System.out.println("Enter a number here: ");
+                while (!sc.hasNextInt()) {
+                    System.out.println("That not a number! Please try again");
+                    sc.next();
+                }
+                command = sc.nextInt();
+            } while (command <= 0);
+           // System.out.println("Thank you! Got " + command);
+                switch (command) {
+                    case 1:
+                        System.out.println("Вы ввели число 1 - Загрузка файла ");
+                        Scanner scName = new Scanner(System.in);
+                        String nameFromKeyBoard = scName.nextLine();
+                        exit = false;
+                        break;
+                    // catch (Exception incorrectInput){
+                    //    System.out.println("IncorrectInput");throw incorrectInput;}
+                    case 2:
+                        System.out.println("You have entered the number 2. Search Persons");
+                        System.out.println("Type a name from the keyboard to search");
+                        Scanner scName2 = new Scanner(System.in);
+                        String Name = scName2.nextLine();
+                        service.searchByName(Name);
+                        exit = false;
+                        break;
+                    case 3:
+                        exit = false;
+                        break;
+                    default:
+                        System.out.println("Error! Please try again:  ");
+                        exit = false;
+                        break;
 
+                } // end switch
 
-                    exit = false;
-                    break;
-                case 2:
-
-                    exit = false;
-                    break;
-                case 3:
-
-                    exit = false;
-                    break;
-                default:
-                    System.out.println("Ошибка! Повторите выбор меню: ");
-                    exit = false;
-                    break;
-            }
         } while (exit != true);
-
-
-
 
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
