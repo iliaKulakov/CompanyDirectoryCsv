f69c3eab621bd9a1176c5bfb2d7ab184c485fd31
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-05-21 22:50:26 +0300

    Added OutputCsvFile method

--- src/main/java/Scanner/OutputCsvFile.java
+++ src/main/java/Scanner/OutputCsvFile.java
@@ -4,20 +4,28 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class OutputCsvFile implements IOutputCsvFile{
+public class OutputCsvFile implements IOutputCsvFile {
 
     public void saveInfoInCsvFileOutput()throws IOException{
 
-       try {
-        BufferedWriter bw = new BufferedWriter(new FileWriter("dbOutput.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter("dbOutput.csv"));
+        try
+        {
             String text = "test.";
             bw.write(text);
         }
-        catch(IOException ex){
+        catch(IOException exc){
 
-            System.out.println(ex.getMessage());
+            System.out.println(exc.getMessage());
+        }
+        finally { // закрытие ресурсов обязательно в finally
+           // Оба вызова обязательно в отдельных try-catch
+           try {
+               bw.close();
+           } catch (IOException ex) {
+               // log here
+           }
         }
 
-
-
-    }}
+    }
+}
